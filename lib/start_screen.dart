910b156ab0dddf60d5e7989f47a7654137cbf930
kirushanth05 <kirushanthvijay@gmail.com>
2024-12-14 21:46:35 +0530

    startScreen UI completed.

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
@@ -6,7 +7,32 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Text('Start Screen',
-            style: TextStyle(color: Colors.white, fontSize: 40)));
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text(
+            'QUIZ TIME',
+            style: TextStyle(color: Colors.white, fontSize: 36),
+          ),
+          const Gap(37),
+          Image.asset(
+            'assets/images/quiz.png',
+            width: 300,
+            height: 300,
+          ),
+          const Gap(83),
+          SizedBox(
+            width: 303,
+            height: 51.19,
+            child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  'Start Quiz',
+                  style: TextStyle(color: Color(0xff01356B), fontSize: 16),
+                )),
+          )
+        ],
+      ),
+    );
   }
 }

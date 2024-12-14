d7cc1b238ed56f2bf1206def0f831667c39c0dc5
kirushanth05 <kirushanthvijay@gmail.com>
2024-12-14 21:45:18 +0530

    Extracted the MaterialApp widget to a new stateful widget and corrected the linear gradient color and it's alignment.

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/start_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(
+    const MainScreen(),
+  );
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({
+    super.key,
+  });
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-          body: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [
-                Colors.blue.shade900, Colors.blue.shade100],
-                )
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Color(0xff01356B), Color(0xff0268D1)],
             ),
-            child: StartScreen()
-            )
-          )));
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
 }

26016d2e462b6776f9e1a76979c60eed5955047d
kirushanth05 <kirushanthvijay@gmail.com>
2024-12-28 23:10:50 +0530

    minor style adjustments.

--- lib/components/custom_elevated_button.dart
+++ lib/components/custom_elevated_button.dart
@@ -2,30 +2,33 @@ import 'package:flutter/material.dart';
 
 class CustomElevatedButton extends StatelessWidget {
   const CustomElevatedButton(
-    this.buttonText,{
-      super.key,
-      });
+    this.buttonText, {
+    super.key,
+  });
 
+  final String buttonText;
 
-final String buttonText;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 51.19,
       width: 303,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           backgroundColor: Colors.white,
         ),
         onPressed: () {},
-        child: Text(
-          buttonText,
-          style: const TextStyle(
-            color: Color(0xff01356B),
-            fontSize: 16,
-          )
-        )
-      )
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 4),
+          child: Text(
+            buttonText,
+            style: const TextStyle(
+              color: Color(0xff01356B),
+              fontSize: 16,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
     );
   }
 }

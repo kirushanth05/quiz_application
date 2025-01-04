6423f33f6c0a1b9d2c4a8bfee37b0d0693c4fc34
kirushanth05 <kirushanthvijay@gmail.com>
2025-01-04 23:27:40 +0530

    The QuizScreen will now display the next question when the user selects an answer also it accepts a function as an argument.

--- lib/quiz_screen.dart
+++ lib/quiz_screen.dart
@@ -3,64 +3,21 @@ import 'package:quiz_app/components/custom_elevated_button.dart';
 import 'package:quiz_app/models/quiz_question.dart';
 import 'package:quiz_app/data/questions.dart';
 
-// List<String> nameList = ['Jude', 'Jeevan', 'Krishanth', 'Vini'];
-// nameList[0]  ==> Jude
-// nameList[2]  ==> Krishanth
-
-/* 
-
-  for (int i = 0; i < nameList.length; i = i + 1) {
-      print(printing namelist);
-  }
-
-  ....
-
-  printing namelist
-  printing namelist
-  printing namelist
-  printing namelist
-
-  printName
-
-  var name = 'Jude'
-  var isAPerson = false
-
-  void printName() {
-    print(name);
-  }
-
-  void runAGivenFunction(Function தரப்பட்டFunction) {
-
-
-      other code....
-
-      தரப்பட்டFunction();
-
-  }
-
-  runAGivenFunction(() {
-    print(name);
-  });
-
-
-
-  nameList.map((value) {
-    print(value)
-  })
-
-*/
-
 class QuizScreen extends StatefulWidget {
-  const QuizScreen({super.key});
+  const QuizScreen({super.key, required this.onQuizComplete});
+
+  final void Function() onQuizComplete;
 
   @override
   State<QuizScreen> createState() => _QuizScreenState();
 }
 
 class _QuizScreenState extends State<QuizScreen> {
+  List<String> answers = [];
+  int index = 0;
   @override
   Widget build(BuildContext context) {
-    QuizQuestion currentQuestion = questions[0];
+    QuizQuestion currentQuestion = questions[index];
     return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
@@ -82,7 +39,19 @@ class _QuizScreenState extends State<QuizScreen> {
           ...currentQuestion.answers.map((String value) {
             return Padding(
               padding: const EdgeInsets.only(bottom: 8.0),
-              child: CustomElevatedButton(value),
+              child: CustomElevatedButton(
+                value,
+                onTap: () {
+                  answers.add(value);
+                  if (index < questions.length - 1) {
+                    setState(() {
+                      index++;
+                    });
+                  } else {
+                    widget.onQuizComplete();
+                  }
+                },
+              ),
             );
           }),
         ],
